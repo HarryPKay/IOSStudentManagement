8886362e5cf4f3475653e3ca3b4b4b7439a46772
Harry Kay <harrykay@harrys-mbp.gateway>
2018-10-21 16:41:07 +1100

    Initial Commit

---
+++ IOSStudentManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSStudentManagement
+//
+//  Created by Harry Kay on 21/10/18.
+//  Copyright © 2018 Harry Kay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
